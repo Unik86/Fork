26c774e95141028715a5eefc1cb411cde7aaf8a7
Kravtsov <kravtsov3d@ukr.net>
2017-08-08 23:13:57 +0300

    fk-03

--- src/main/java/com/fork/Mian.java
+++ src/main/java/com/fork/Mian.java
@@ -1,21 +1,35 @@
 package com.fork;
 
-import com.fork.parser.FavbetParser;
-import com.fork.parser.Parser;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.IOException;
+import java.util.List;
 
 public class Mian {
 
     public static void main(String[] args) {
-        Parser parser = new Parser("http://sports.williamhill.com/bet/ru/betlive/24");
-        parser.pars();
-        parser.print();
+//        Parser parser = new Parser("http://sports.williamhill.com/bet/ru/betlive/24");
+//        parser.pars();
+//        parser.print();
 
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://new.favoritsport.com.ua/ru/live/#sports=1,2");
 
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        List<WebElement> allAuthors =  driver.findElements(By.className("outcome"));
+
+        for (WebElement row : allAuthors){
+            System.out.println(row.getText());
+        }
+
+        driver.close();
 
     }
 
