ab139beaecd890dfec05cdceedbca4917469f865
Kravtsov <kravtsov3d@ukr.net>
2017-08-11 22:23:44 +0300

    fk-09

--- src/main/java/com/fork/Mian.java
+++ src/main/java/com/fork/Mian.java
@@ -1,24 +1,34 @@
 package com.fork;
 
-import com.fork.parser.FavMatchParser;
-import com.fork.parser.Parser;
-import com.fork.parser.WillMatchParser;
+import com.fork.model.Fork;
 
 public class Mian {
 
-    public static String WILL_URL = "http://sports.williamhill.com/bet/ru/betting/e/11262526/%D0%91%D0%B5%D1%80%D0%B8+v+%D0%A1%D0%B0%D0%BD%D0%B4%D0%B5%D1%80%D0%BB%D0%B5%D0%BD%D0%B4.html";
+    public static String WILL_URL = "http://sports.williamhill.com/bet/ru/betting/e/11529279/Atletico+Guanare+v+Club+Deportivo+Lara.html";
     public static String FAV_URL = "https://www.favbet.com/ru/live/#event=9235378";
 
     public static void main(String[] args) {
 
-        Parser will = new WillMatchParser(WILL_URL);
-        will.pars();
-        will.print();
+//        Parser will = new WillMatchParser(WILL_URL);
+//        will.pars();
+//        will.print();
 
 //        Parser fav = new FavMatchParser(FAV_URL);
 //        fav.pars();
 //        fav.print();
 
+        calc();
+    }
+
+
+    public static void calc(){
+        Double rate1 = 2.9D;
+        Double rate2 = 1.61D;
+        Double allSum = 100D;
+
+        Fork fork = new Fork(rate1, rate2, allSum);
+        fork.calc();
+        fork.print();
     }
 
 }
--- src/main/java/com/fork/util/Utils.java
+++ src/main/java/com/fork/util/Utils.java
@@ -6,4 +6,8 @@ public class Utils {
         return String.format("%1$"+length+ "s", string);
     }
 
+    public static Double round(Double value){
+        return Math.round( value * 100.0 ) / 100.0;
+    }
+
 }
